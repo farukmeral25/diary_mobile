18082980b454130f6df4cd5af8bf92a5176368f1
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-04-08 23:50:13 +0300

    refactor: update main function to initialize environment and dependencies asynchronously

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:diary_mobile/app.dart';
+import 'package:diary_mobile/core/constant/app_environment.dart';
 import 'package:flutter/material.dart';
+import 'package:diary_mobile/core/init/injection_container.dart' as locator;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.wait([dotenv.load(fileName: AppEnvironment().fileName), locator.init()]);
   runApp(const DiaryApp());
 }
